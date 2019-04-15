8a6e79e7a4f941b0f8936368af89a7092014852c
Gekif <dzulfikar.maulana@gmail.com>
2019-04-15 09:19:03 +0700

    02-Basics: Constants

---
+++ 02-Basics/constant.js
@@ -0,0 +1,18 @@
+// Temporary variable
+// let interestRate = 0.3;
+
+// Variable can be change
+// interestRate = 1;
+
+// Print the Change
+// console.log(interestRate);
+
+
+// Constant cannot be change, constant should be best choice
+const interestRate = 0.3;
+
+// Variable can be change
+// interestRate = 1;
+
+// Print the Change
+console.log(interestRate);
