00e2b39242e9013513f20e7cdaff4e537fea7a7a
Gekif <dzulfikar.maulana@gmail.com>
2019-04-15 09:16:32 +0700

    02-Basics: Variable

---
+++ 02-Basics/variable.js
@@ -0,0 +1,31 @@
+
+// Declare Variable and Define It
+let name = 'Fikar';
+
+// Print Variable
+console.log(name);
+
+
+// Notes of Variable
+/*
+1. Cannot be a reserved keyword
+2. Should be meaningful
+3. Cannot start with a number (ex 1name)
+4. Cannot contain a space or hyphen(-)
+4. Are case sensitive
+ */
+
+
+// Example of camel case
+// let firstName;
+
+
+// Example of multiple variable declaration in one line
+// let firstName = 'Fikar', lastName = 'Maulana';
+
+// But this is the best practice
+let firstName = 'Fikar';
+let lastName = 'Maulana';
+
+// Print the multiple lines
+console.log(`Hello ${firstName} i know you're Last Name is ${lastName}`)
