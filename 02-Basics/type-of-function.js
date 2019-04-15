6c0b7c282eb990f24fe846a7f372ca2d0e00fd4f
Gekif <dzulfikar.maulana@gmail.com>
2019-04-15 09:58:21 +0700

    02-Basics: Type Of Functions

---
+++ 02-Basics/type-of-function.js
@@ -0,0 +1,28 @@
+
+// Performing a task
+function greet(firstName, lastName) {
+    console.log('Hello ' + firstName + ' ' + lastName);
+}
+
+// Display something
+greet('Lets', 'Dance');
+
+
+// Calculating a value
+function square(number) {
+
+    // return is for numeric
+    return number * number;
+}
+
+// Give the argument of function
+// square(2);
+
+// Define in variable
+// let number = square(2);
+
+// Print the function
+// console.log(number);
+
+// or do this -> Same result
+console.log(square(2));
