9d1fd9afb9433d3e974aaf61a9df17871afda41c
Gekif <dzulfikar.maulana@gmail.com>
2019-04-15 15:44:37 +0700

    03-Operators: Equality Operators

---
+++ 03-Operators/EqualityOperator.js
@@ -0,0 +1,24 @@
+
+/**
+ * Strict Equality ( Type + Value
+ */
+
+// Return True
+console.log(1 === 1);
+
+// Return False
+console.log('1' === 1);
+
+
+/**
+ * Lose Equality
+ */
+
+// Return True
+console.log(1 == 1);
+
+// Return True. Automatically convert to number
+console.log('1' == 1);
+
+// Return True. Convert number to boolean
+console.log(true == 1);
