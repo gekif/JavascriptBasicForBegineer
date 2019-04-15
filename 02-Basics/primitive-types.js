a175056339db801c95c6aaaef31806071fa14650
Gekif <dzulfikar.maulana@gmail.com>
2019-04-15 09:22:19 +0700

    02-Basics: Primitive Types

---
+++ 02-Basics/primitive-types.js
@@ -0,0 +1,17 @@
+// String literal
+let name = 'Fikar';
+
+// Number literal
+let age = 30;
+
+// Boolean literal (can be true or false)
+let isApproved = true;
+
+// Dont have value it means undefined
+let firstName;
+
+// set null value
+let selectedColor = null;
+
+// in the future
+selectedColor = 'Red';
