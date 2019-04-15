c4ac8c67252a9eaeb67d9cc1c223f046017849aa
Gekif <dzulfikar.maulana@gmail.com>
2019-04-15 09:44:29 +0700

    02-Basics: Array

---
+++ 02-Basics/array.js
@@ -0,0 +1,33 @@
+
+// Declaration Array
+let selectedColors = [ 'red', 'blue' ];
+
+// Print Array
+// console.log(selectedColors);
+
+//Print Specific Array
+// console.log(selectedColors[0]);
+
+
+// Add array
+// selectedColors[2] = 'green';
+// selectedColors[1] = '1';
+
+// Print Array After Add it
+// console.log(selectedColors);
+
+
+// Change array
+// selectedColors[0] = 'Kiki';
+
+// Print Changed Array
+// console.log(selectedColors);
+// console.log(selectedColors[0]);
+
+
+/**
+ * Array Properties
+ */
+
+// length
+console.log(selectedColors.length);
