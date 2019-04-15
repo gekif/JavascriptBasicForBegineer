750b9e80536fa2c64d9a7dfa7c5fc0bed51186da
Gekif <dzulfikar.maulana@gmail.com>
2019-04-15 15:36:06 +0700

    03-Operators: Assignment Operators

---
+++ 03-Operators/AssigmentOperators.js
@@ -0,0 +1,19 @@
+
+// Declare x
+let x = 10;
+
+/**
+ * The Operation
+ */
+
+// Similar with x = x + 1
+x++;
+
+// Similar with x = x + 5
+x += 5;
+
+// Similar with x = x * 3
+x *= 3;
+
+// Print x
+console.log(x); // Print 11
