61d995f875b277ac9c6145bb37a7984e5f459f68
Gekif <dzulfikar.maulana@gmail.com>
2019-04-15 15:40:05 +0700

    03-Operators: Comparison Operators

---
+++ 03-Operators/ComparisonOperators.js
@@ -0,0 +1,33 @@
+
+// Declare x
+let x = 1;
+
+
+/**
+ * Relational Operator
+  */
+
+// Check if x more than 0. Return true
+console.log(x > 0);
+
+// x greater than
+console.log(x >= 1);
+
+// x less than
+console.log(x < 1);
+
+// x less than similar
+console.log(x <= 1);
+
+
+/**
+ * Equality Operator
+ */
+
+// Check if x is very true
+console.log(x === 1);
+
+// Check if x is not very true
+console.log(x !== 1);
+
+
