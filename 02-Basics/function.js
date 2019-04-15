667a096aa8fab143f0169f2083dba46a497163fe
Gekif <dzulfikar.maulana@gmail.com>
2019-04-15 09:54:02 +0700

    02-Basics: Functions

---
+++ 02-Basics/function.js
@@ -0,0 +1,64 @@
+
+// Javascript Function
+// function greet() {
+//     console.log('Hello World');
+// }
+
+// Call javascript function
+// greet();
+
+
+// Function with parameter
+/*function greet(name) {
+    console.log('Hello ' + name);
+}*/
+
+// Calling function with argument
+// greet('Bebek');
+// greet('Mandra');
+
+
+// Function with parameter
+/*function greet(firstName, lastName) {
+    console.log('Hello ' + firstName + '. Your last name is ' + lastName);
+}*/
+
+// Calling function with argument
+/*
+greet('Bebek', 'Mandra');
+greet('Doel', 'Sumbang');
+*/
+
+
+// Ecmascript Function
+/*
+const greet = () => {
+    console.log(`Hello World`);
+};
+*/
+
+// Calling ES6 Function
+// greet();
+
+
+// Ecmascript Function With Parameter
+/*const greet = (name) => {
+    console.log(`Hello ${name}`);
+};*/
+
+
+// Calling Ecmascript function with argument
+/*
+greet('Bebek');
+greet('Mandra');
+*/
+
+
+// Ecmascript Function With Multiple Parameter
+const greet = (firstName, lastName) => {
+    console.log(`Hello ${firstName}. Your last name is ${lastName}`);
+};
+
+// Calling Ecmascript function with argument
+greet('Bebek', 'Mandra');
+greet('Doel', 'Sumbang');
