4e389eb0b3927d72373f820d510e817966a24f0c
Gekif <dzulfikar.maulana@gmail.com>
2019-04-16 11:05:47 +0700

    04-Control Flow: Do While

---
+++ 04-Control-Flow/DoWhile.js
@@ -0,0 +1,38 @@
+/**
+ * Convensional Do While
+ * @type {number}
+ */
+/*let i = 1;
+
+do {
+    console.log(i);
+    i++;
+}while( i < 5);*/
+
+
+/**
+ * Odd Number Do While
+ * Ascending
+ * @type {number}
+ */
+/*
+let i = 1;
+
+do {
+    if(i % 2 !== 0) console.log(i);
+    i++;
+}while( i <= 5);
+*/
+
+
+/**
+ * Odd Number Do While
+ * Descending
+ * @type {number}
+ */
+let i = 5;
+
+do {
+    if(i % 2 !== 0) console.log(i);
+    i--;
+}while( i >= 1);
