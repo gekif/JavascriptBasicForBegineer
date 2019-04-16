44e0df02b000e513610d8d366a9286f0c1ba589a
Gekif <dzulfikar.maulana@gmail.com>
2019-04-16 10:56:16 +0700

    04-Control Flow: For

---
+++ 04-Control-Flow/For.js
@@ -0,0 +1,35 @@
+/**
+ * Example 1
+ */
+/*for (let i = 1; i <= 5; i++) {
+
+    // Print only the repeat word
+    // console.log('Anjing');
+
+    // Print using index
+    console.log(`${i}. Anjing Elu`);
+}*/
+
+
+/**
+ * Example 2
+ * Print Odd Number
+ * Ascending
+ */
+/*for (let i = 1; i <= 5; i++) {
+
+    // Print the Odd Number
+    if (i %2 !== 0) console.log(i);
+}*/
+
+
+/**
+ * Example 3
+ * Print Odd Number
+ * Descending
+ */
+for (let i = 5; i >= 1; i--) {
+
+    // Print the Odd Number
+    if (i %2 !== 0) console.log(i);
+}
