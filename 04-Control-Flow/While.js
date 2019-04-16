4a9a984b486db8ed09fcdcd9e0893f6dc1290769
Gekif <dzulfikar.maulana@gmail.com>
2019-04-16 11:02:23 +0700

    04-Control Flow: While

---
+++ 04-Control-Flow/While.js
@@ -0,0 +1,37 @@
+/**
+ * Conventional while looping
+ * @type {number}
+ */
+/*let i = 1;
+
+while (i <= 5) {
+    console.log(i);
+    i++;
+}*/
+
+
+/**
+ * Print Odd Number
+ * Ascending
+ * @type {number}
+ */
+/*
+let i = 0;
+
+while (i <= 5) {
+    if (i % 2 !== 0) console.log(i);
+    i++;
+}*/
+
+
+/**
+ * Print Odd Number
+ * Descending
+ * @type {number}
+ */
+let i = 5;
+
+while (i >= 1) {
+    if (i % 2 !== 0) console.log(i);
+    i--;
+}
