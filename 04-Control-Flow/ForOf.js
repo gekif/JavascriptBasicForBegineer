bcadff7c6f2bbdae51a06a5dac65fdbb21c04343
Gekif <dzulfikar.maulana@gmail.com>
2019-04-18 15:43:31 +0700

    04-Control Flow: For Of

---
+++ 04-Control-Flow/ForOf.js
@@ -0,0 +1,14 @@
+/**
+ * Only work in ecmascript
+ */
+
+const colors = ['red', 'green', 'blue'];
+
+
+for (let color of colors)
+
+    // Print only the value
+    console.log(color);
+
+    // Print all array
+    console.log(colors);
