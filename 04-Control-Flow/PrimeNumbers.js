47240e8447e44520826e9c578856c6897467c26e
Gekif <dzulfikar.maulana@gmail.com>
2019-04-24 16:08:13 +0700

    04-Control Flow: Prime Numbers 2

--- 04-Control-Flow/PrimeNumbers.js
+++ 04-Control-Flow/PrimeNumbers.js
@@ -3,15 +3,29 @@ showPrimes(100);
 
 function showPrimes(limit) {
     for (let number = 2; number <= limit; number++) {
-        let isPrime = true;
+
+        // Before Extracted
+        /*let isPrime = true;
 
         for (let factor = 2; factor < number; factor++) {
             if (number % factor === 0) {
                 isPrime = false;
                 break;
             }
-        }
+        }*/
 
-        if (isPrime) console.log(number);
+        // First Print
+        // if (isPrime) console.log(number);
+
+        // Second Print
+        if (isPrime(number)) console.log(number);
     }
+}
+
+function isPrime(number) {
+    for (let factor = 2; factor < number; factor++)
+        if (number % factor === 0)
+            return false;
+
+    return true;
 }
