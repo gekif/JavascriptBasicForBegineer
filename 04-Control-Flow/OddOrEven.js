8195a60f3f36f270e3e09486402c4d968794a91d
Gekif <dzulfikar.maulana@gmail.com>
2019-04-24 10:00:34 +0700

    04-Control Flow: Odd Or Even Numbers 2

--- 04-Control-Flow/OddOrEven.js
+++ 04-Control-Flow/OddOrEven.js
@@ -3,10 +3,21 @@ showNumbers(10);
 function showNumbers(limit) {
 
     for (let i = 0; i <= limit; i++) {
-        if (i % 2 === 0) {
+
+        // First Method if else
+        /*if (i % 2 === 0) {
             console.log(i, 'EVEN');
         } else {
             console.log(i, 'ODD');
-        }
+        }*/
+
+        // Second Method if else
+        if (i % 2 === 0) console.log(i, 'EVEN');
+        else console.log(i, 'ODD');
+
+        // Ternary Operator
+        const message = (1 % 2 === 0) ? 'EVEN' : 'ODD';
+        console.log(i, message)
+
     }
 }
