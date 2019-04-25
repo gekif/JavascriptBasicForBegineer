1c516ee9e67f53b34fb028fecff9f93c3f8fe291
Gekif <dzulfikar.maulana@gmail.com>
2019-04-25 16:21:31 +0700

    05-Objects: Basics

---
+++ 05-Objects/Basics.js
@@ -0,0 +1,17 @@
+// Object Oriented Programming
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    isVisible: true,
+    draw: function () {
+        console.log('draw')
+    },
+};
+
+// Access Object
+circle.draw();
+
+
