1f3bc639ba8f248ac4a76c5af0bceb629a159207
Gekif <dzulfikar.maulana@gmail.com>
2019-05-03 11:43:42 +0700

    05-Objects: Function of Objects

---
+++ 05-Objects/FunctionObjects.js
@@ -0,0 +1,21 @@
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function () {
+        console.log('draw');
+    }
+}
+
+const Circle1 = new Function('radius', `
+    this.radius = radius;
+    this.draw = function () {
+        console.log('draw');
+    }
+`);
+
+/*const circle = new Circle(1);
+console.log(circle);*/
+
+Circle.call({}, 1);
+Circle.apply({}, [1, 2, 3]);
+
+const another = new Circle(1);
