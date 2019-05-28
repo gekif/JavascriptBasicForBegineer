740351048e04894509572a5c9e50811a065d1b7b
Gekif <dzulfikar.maulana@gmail.com>
2019-05-28 15:32:06 +0700

    05-Objects: Math

---
+++ 05-Objects/Math.js
@@ -0,0 +1,9 @@
+let random = Math.random();
+let round = Math.round(1.9);
+let max = Math.max(1, 2, 3, 4, 5);
+let min = Math.min(1, 2, 3, 4, 5);
+
+// console.log(random);
+// console.log(round);
+// console.log(max);
+console.log(min);
