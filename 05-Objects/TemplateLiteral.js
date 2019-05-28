59c24ad5de35b5756299a864cfa10b4582561ff2
Gekif <dzulfikar.maulana@gmail.com>
2019-05-28 16:08:11 +0700

    05-Objects: Template Literals

---
+++ 05-Objects/TemplateLiteral.js
@@ -0,0 +1,40 @@
+// String primitive
+// const message =
+//     'This is my\n' +
+//     '\'first\' message';
+
+// console.log(message);
+
+
+
+// Object{}
+// Boolean true, false
+// String ''. ""
+// Template ``
+
+
+
+// const another =
+// `This is my
+// 'first' message`;
+//
+// console.log(another);
+
+
+
+/**
+ * Email template
+ */
+const receiver = 'Fikar';
+
+const email =
+`Hi ${receiver} ${2 + 3},
+
+Thank you for joining my mailing list.
+
+Regards,
+Mosh`;
+
+
+console.log(email);
+
