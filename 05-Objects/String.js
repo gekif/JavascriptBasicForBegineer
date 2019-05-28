30aa5a1b6ba227956a8ecc8f78135d975a875c93
Gekif <dzulfikar.maulana@gmail.com>
2019-05-28 15:51:29 +0700

    05-Objects: String

---
+++ 05-Objects/String.js
@@ -0,0 +1,63 @@
+/**
+ * String primitive
+ */
+
+// Declaration
+// const message = 'hi';
+const message = 'This is my first message';
+// const message = 'This is my first message               ';
+// const message = 'This is my \'first message';
+// const message = 'This is my first \nmessage';
+
+//Length of properties
+// console.log(message.length);
+
+//Print index of zero
+// console.log(message[0]);
+
+// Print index of one
+// console.log(message[1]);
+
+// Check the include word
+// console.log(message.includes('my')); // true
+// console.log(message.includes('wong jowo')); // false
+
+// Check the start word
+// console.log(message.startsWith('This')); // true
+// console.log(message.startsWith('this')); // false
+
+// Check the end word
+// console.log(message.endsWith('e')); // true
+// console.log(message.endsWith('n')); // false
+
+// Check the index
+// console.log(message.indexOf('my'));
+
+// Replace string
+// console.log(message.replace('first', 'second'));
+
+// Upper case
+// console.log(message.toUpperCase());
+
+// Lower case
+// console.log(message.toLowerCase());
+
+// Trim message
+// console.log(message.trim()); // trim left and right
+// console.log(message.trimLeft()); // trim only left
+// console.log(message.trimRight()); // trim only right
+
+// Print single quote
+// console.log(message);
+
+// Print new line
+// console.log(message);
+
+// Split message
+console.log(message.split(' '));
+
+
+
+// String object
+// const another = new String('hi');
+
