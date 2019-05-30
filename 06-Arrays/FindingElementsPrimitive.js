dc10b5d6458b9729561523355f7601fe8a04501e
Gekif <dzulfikar.maulana@gmail.com>
2019-05-30 11:02:55 +0700

    06-Arrays: Finding Elements Primitive

---
+++ 06-Arrays/FindingElementsPrimitive.js
@@ -0,0 +1,20 @@
+// const numbers = [1, 2, 3, 4];
+const numbers = [1, 2, 3, 1, 4];
+
+// Find the number from index
+// console.log(numbers.indexOf(1));
+
+// -1 False
+// console.log(numbers.indexOf(0));
+
+// Last Index
+// console.log(numbers.lastIndexOf(1));
+
+// Using equality operator
+// console.log(numbers.indexOf(1) !== -1);
+
+// Another method, the result same as above
+// console.log(numbers.includes(1));
+
+// Passing another argument
+console.log(numbers.indexOf(1, 2));
