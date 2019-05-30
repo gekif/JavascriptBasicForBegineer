dd9c71349b6d9bfea17e16c4700285e79253c4f6
Gekif <dzulfikar.maulana@gmail.com>
2019-05-30 12:24:34 +0700

    06-Arrays: Testomg Elements

---
+++ 06-Arrays/TestingElements.js
@@ -0,0 +1,18 @@
+// positive number
+// const numbers = [1, 2, 3];
+
+// false number
+const numbers = [-1, 1, 2, 3];
+
+// const allPositive = numbers.every(value => value >= 0);
+
+const atLeastOnePositive = numbers.some(value => value >= 0);
+
+
+// console.log(allPositive);
+
+console.log(atLeastOnePositive);
+
+
+// every()
+// some()
