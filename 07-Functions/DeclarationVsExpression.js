3bcee6727aa7ee03fbb2711dd56bc486a20ac03c
Gekif <dzulfikar.maulana@gmail.com>
2019-06-01 09:32:02 +0700

    07-Functions: Function Declaration vs Function Expressions

---
+++ 07-Functions/DeclarationVsExpression.js
@@ -0,0 +1,17 @@
+// Function Declaration
+function walk() {
+    console.log('walk');
+}
+
+// Function Expresion
+let run = function () {
+    console.log('run');
+};
+
+// Declare variable to set function
+let move = run;
+move();
+
+
+/*walk();
+run();*/
